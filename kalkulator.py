e88174fa4b86891d763d559510e97a0ac57027ac
szymciu89 <aaa@wp.pl>
2014-05-14 10:48:00 +0200

    dodalem funkcje get_info i zintegrowalem galezie

--- kalkulator.py
+++ kalkulator.py
@@ -2,4 +2,8 @@ def dodawanie():
     a=int(input("wprowadz pierwsza liczbe"))
     b=int(input("wprowadz druga liczbe"))
     print(a+b)
+def get_info():
+    print('ten program dodaje dwie liczby')
+    
+get_info()        
 dodawanie()    
